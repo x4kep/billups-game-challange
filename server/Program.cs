1f06ad02de825c869250d2eb6050ce09f01f660a
Dusan Veselinovic <dusan.veselinovic.dev@gmail.com>
2025-08-16 12:20:58 +0200

    test: build and docker artifact

--- server/Program.cs
+++ server/Program.cs
@@ -9,7 +9,7 @@ builder.WebHost.ConfigureKestrel(options =>
     options.ListenAnyIP(5209);
 });
 
-// Add services to the container
+// Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
